58b0eceb64b8997860e693c8d9097aeb3f9047a7
Saint Lord <thesaint@Saints-MacBook-Pro.local>
2023-04-29 02:50:43 +0600

    Chat GPT corrected version added as comment.

--- index.js
+++ index.js
@@ -54,3 +54,58 @@ app.get('/categories/:id', (req, res) => {
     }
 
 })
+
+/**
+ * Chat GPT version
+ */
+
+/**
+ * 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Dragon News Server');
+});
+
+app.get('/categories', (req, res) => {
+  res.send(categories);
+});
+
+// Moved the following two routes inside app.listen to avoid "Cannot GET /news" error
+app.get('/news', (req, res) => {
+  res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+
+  const specificNews = news.find((newsItem) => newsItem._id === id);
+  res.send(specificNews);
+});
+
+app.listen(port, () => {
+  console.log(`Dragon News Server listening on port ${port}`);
+  
+  app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+
+    if (id === 0) {
+      res.send(news);
+    } else {
+      const specificCategory = news.filter((newsItem) => parseInt(newsItem.category_id) === id);
+      res.send(specificCategory);
+    }
+  });
+});
+
+ */
