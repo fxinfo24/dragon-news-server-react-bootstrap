0e4b7c6eddfeb34c914bbd58817a1bf8c43729e9
Saint Lord <thesaint@Saints-MacBook-Pro.local>
2023-04-29 01:34:31 +0600

    news.json for news detail added, get news and category by id implemented.

--- index.js
+++ index.js
@@ -20,3 +20,30 @@ app.get ( '/categories', (req, res) => {
 app.listen(port, () => {
     console.log(`Dragon news Server listening on port, ${port}`)
 });
+
+// Module 63-2 (News Details)
+
+const news =require('./data/news.json')
+
+app.get( '/news', (req, res) => {
+    res.send(news);
+})
+
+// For getting specific news details with specific id:
+app.get( '/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+
+    // Get specific news with specific id using 'find' method; cause we need to 'find' the news with specific id.
+    const specificNews = news.find(idNumber => idNumber._id === id);
+    res.send(specificNews);
+})
+
+// Get specific "category's" news with specific 'category id' using 'filter' method; cause more than one news may exist in a single category.
+app.get('/categories:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id); // id number as number not as string
+
+    const specificCategory = category.filter(idNumber => parseInt(idNumber.category_id) === id);
+    res.send(specificCategory);
+})
